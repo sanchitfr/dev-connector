000e5e735a0f7bff2835ce0929eb053f088e3743
Sanchit <sanchit522@gmail.com>
2020-09-10 00:32:08 +0530

    added react frontend with landing, login and register pages

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,49 @@
+import React, {Fragment, useState} from 'react';
+import { Link } from 'react-router-dom';
+
+const Login = () => {
+
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  });
+
+  const { email, password } = formData;
+
+
+  const handleChange = e => setFormData({ ...formData, [e.target.name] : e.target.value });
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+      console.log('Success');
+  }
+
+    return (
+        <Fragment>
+          <h1 className="large text-primary">Sign Up</h1>
+            <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
+            <form className="form" onSubmit={e => handleSubmit(e)}>
+              <div className="form-group">
+                <input type="email" placeholder="Email Address" name="email" value={email} onChange={e => handleChange(e)} required/>
+              </div>
+              <div className="form-group">
+                <input
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                  minLength="6"
+                  value={password} 
+                  onChange={e => handleChange(e)}
+                />
+              </div>
+              <input type="submit" className="btn btn-primary" value="Login" />
+            </form>
+            <p className="my-1">
+              Don't have an account? <Link to="/register">Sign Up</Link>
+            </p>  
+        </Fragment>
+    )
+};
+
+export default Login;
+
